d1345e3cb9cd71b9c75b63f49860260459a367b7
Federico Busetti <729029+febus982@users.noreply.github.com>
2022-11-09 18:50:26 +0000

    Use random-named db for test parallelisation

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,13 @@
+import os
+from uuid import uuid4
+
 import pytest
 from fastapi import FastAPI
 from dependency_injector import providers
 from sqlalchemy.orm import clear_mappers
 
 from app import create_app, Container, AppConfig
-from app.deps.sqlalchemy_manager import SQLAlchemyBindConfig
+from app.deps.sqlalchemy_manager import SQLAlchemyBindConfig, SQLAlchemyManager
 
 
 @pytest.fixture(scope="function")
@@ -18,10 +21,11 @@ def anyio_backend():
 
 @pytest.fixture(scope="function")
 def app() -> FastAPI:
+    test_db_path = f"./{uuid4()}.db"
     test_config = AppConfig(
         SQLALCHEMY_CONFIG={
             "default": SQLAlchemyBindConfig(
-                engine_url="sqlite:///./test.db",
+                engine_url=f"sqlite:///{test_db_path}",
                 engine_options=dict(connect_args={"check_same_thread": False}),
             ),
         }
@@ -29,5 +33,11 @@ def app() -> FastAPI:
     # TODO: better test config management (perhaps remove from DI container)
     c = Container()
     c.config.override(providers.Object(test_config))
+
+    sa_manager: SQLAlchemyManager = c.SQLAlchemyManager()
+    for k, v in sa_manager.get_binds().items():
+        v.registry_mapper.metadata.create_all(v.engine)
+
     yield create_app(di_enabled=False)
     clear_mappers()
+    os.unlink(test_db_path)
