fd5d1e05ff55b57b751f514c0b38694ee79d623d
Federico Busetti <729029+febus982@users.noreply.github.com>
2022-11-09 19:56:52 +0000

    Remove test endpoints

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,8 @@
-from attrs import asdict
 from fastapi import FastAPI
 
 from app.config import AppConfig
 from app.containers import Container
 from app.deps.sqlalchemy_manager import SQLAlchemyManager
-from app.models import User, Address
 from app.routes import init_routes
 from app.storage import init_storage
 
@@ -15,7 +13,7 @@ def create_app(
 ) -> FastAPI:
     if di_enabled:
         # Initialise and wire DI container
-        c = Container()
+        Container()
 
     if test_config:
         app = FastAPI(debug=True)
@@ -24,28 +22,5 @@ def create_app(
 
     init_storage()
     init_routes(app)
-
-    @app.get('/new')
-    def new():
-        user = User(name="pippo")
-        a = Address(name="aaa", user_id=user.user_id)
-
-        with c.SQLAlchemyManager().get_session() as session:
-            session.add(user)
-            session.add(a)
-            session.commit()
-            user_id = user.user_id
-            address_id = a.address_id
-            print(user_id, address_id)
-            user_from_db = session.query(User).get(user_id)
-            a_db = session.query(Address).get(address_id)
-        return {"user": asdict(user_from_db), "address": asdict(a_db)}
-
-    @app.get('/get/{id}')
-    def get(id: int):
-        with c.SQLAlchemyManager().get_session() as session:
-            user_from_db = session.get(User, id)
-            print(str(type(user_from_db)))
-        return {"user_type": str(type(user_from_db))}
 
     return app
