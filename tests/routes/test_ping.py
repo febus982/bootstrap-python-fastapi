705ae355142b7245a681447463f419c0189af667
Federico Busetti <729029+febus982@users.noreply.github.com>
2022-12-11 23:54:35 +0000

    Remove duplicate test

--- tests/routes/test_ping.py
+++ tests/routes/test_ping.py
@@ -8,11 +8,3 @@ async def test_root(testapp):
         response = await ac.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"ping": "pong!"}
-
-
-@pytest.mark.anyio
-async def test_root2(testapp):
-    async with AsyncClient(app=testapp, base_url="http://test") as ac:
-        response = await ac.get("/ping")
-    assert response.status_code == 200
-    assert response.json() == {"ping": "pong!"}
