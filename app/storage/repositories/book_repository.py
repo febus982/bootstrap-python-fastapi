43166e471ed1ef73c384a3de1303f058f4b3fb1f
Federico Busetti <729029+febus982@users.noreply.github.com>
2022-12-28 18:11:17 +0100

    Switch to the generic SQLAlchemyRepository implementation

--- app/storage/repositories/book_repository.py
+++ app/storage/repositories/book_repository.py
@@ -1,15 +1,15 @@
+from dependency_injector.wiring import Provide, inject
+from sqlalchemy_bind_manager import SQLAlchemyRepository, SQLAlchemyBindManager
+
 from app.domains.books._local import BookRepositoryInterface, BookModel
-from .abstract import SQLAlchemyRepository
 
 
-class BookRepository(BookRepositoryInterface, SQLAlchemyRepository):
-    def save(
-        self,
-        book: BookModel,
-    ) -> BookModel:
-        # MyPy doesn't like session used as context manager
-        with self.sa_manager.get_session() as session:  # type: ignore
-            session.add(book)
-            session.commit()
+class BookRepository(SQLAlchemyRepository[BookModel], BookRepositoryInterface):
+    _model = BookModel
 
-        return book
+    @inject
+    def __init__(
+            self,
+            sa_manager: SQLAlchemyBindManager = Provide[SQLAlchemyBindManager.__name__],
+    ) -> None:
+        super().__init__(sa_manager)
