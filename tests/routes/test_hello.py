eebea3ba2bc2bc1468fdf3087a3d4c037153dbcd
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-01-04 20:43:29 +0000

    Remove comment

--- tests/routes/test_hello.py
+++ tests/routes/test_hello.py
@@ -7,4 +7,3 @@ async def test_root(testapp):
     async with AsyncClient(app=testapp, base_url="http://test") as ac:
         response = await ac.get("/hello/")
     assert response.status_code == 200
-    # assert response.json() == {"ping": "pong!"}
