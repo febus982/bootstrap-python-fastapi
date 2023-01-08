c276576a05b9f633ab4004eb1459d3a6a013ab6a
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-01-08 01:27:44 +0000

    Handle pytest issue

--- grpc_app/__init__.py
+++ grpc_app/__init__.py
@@ -1,3 +1,4 @@
+import logging
 from concurrent import futures
 
 import grpc
--- grpc_app/__main__.py
+++ grpc_app/__main__.py
@@ -3,14 +3,16 @@ import time
 
 from grpc_app import create_server
 
-_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+# TODO: Double-check this if statement, without it check pytest hangs.
+if __name__ == "__main__":  # pragma: no cover
+    _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
-server = create_server()
-server.start()
-logging.info("GRPC started")
-try:
-    while True:
-        time.sleep(_ONE_DAY_IN_SECONDS)
-except KeyboardInterrupt:
-    logging.debug("GRPC stop")
-    server.stop(0)
+    server = create_server()
+    server.start()
+    logging.info("GRPC started")
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        logging.debug("GRPC stop")
+        server.stop(0)
