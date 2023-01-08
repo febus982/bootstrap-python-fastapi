02d60bb800386ff7c3d5fdec0aca4f0da9499d22
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-01-08 11:57:20 +0000

    Add test config support to grpc server factory and pytest

--- grpc_app/__init__.py
+++ grpc_app/__init__.py
@@ -1,5 +1,6 @@
 import logging
 from concurrent import futures
+from typing import Optional
 
 import grpc
 from dependency_injector.providers import Object
@@ -12,19 +13,20 @@ from storage import init_storage
 
 
 # TODO: Add asyncio support: https://stackoverflow.com/questions/38387443/how-to-implement-a-async-grpc-python-server
-def create_server():
-    Container(  # type: ignore
-        config=Object(AppConfig()),
+def create_server(test_config: Optional[AppConfig] = None):
+    c = Container(
+        config=Object(test_config or AppConfig()),
     )
     init_storage()
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    server.container = c   # type: ignore
     # Register service
     add_BooksServicer_to_server(BooksService(), server)
     server.add_insecure_port("[::]:50051")
     return server
 
 
-def main():
+def main():  # pragma: no cover
     server = create_server()
     server.start()
     logging.info("GRPC started")
---
+++ tests/grpc_app/conftest.py
@@ -0,0 +1,34 @@
+import os
+from uuid import uuid4
+
+import pytest
+from grpc import Server
+from sqlalchemy.orm import clear_mappers
+from sqlalchemy_bind_manager import SQLAlchemyBindConfig
+
+from config import AppConfig
+from grpc_app import create_server
+
+
+@pytest.fixture(scope="function")
+def testserver() -> Server:
+    test_db_path = f"./{uuid4()}.db"
+    clear_mappers()
+    test_config = AppConfig(
+        SQLALCHEMY_CONFIG={
+            "default": SQLAlchemyBindConfig(
+                engine_url=f"sqlite:///{test_db_path}",
+                engine_options=dict(connect_args={"check_same_thread": False}),
+                session_options=dict(expire_on_commit=False),
+            ),
+        }
+    )
+
+    server = create_server(test_config=test_config)
+    sa_manager = server.container.SQLAlchemyBindManager()
+    for k, v in sa_manager.get_binds().items():
+        v.registry_mapper.metadata.create_all(v.engine)
+
+    yield server
+    os.unlink(test_db_path)
+    clear_mappers()
