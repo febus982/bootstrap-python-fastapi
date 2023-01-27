4c3368710f542be21bd33989e3de0d80eb6b7d95
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-01-27 15:00:53 +0000

    Check for bind type during repository initialisation

--- tests/storage/repositories/test_book_repository.py
+++ tests/storage/repositories/test_book_repository.py
@@ -1,5 +1,17 @@
+from unittest.mock import MagicMock
+
+import pytest
+from sqlalchemy_bind_manager.exceptions import UnsupportedBind
+
 from domains.books._local import BookModel
 from storage.repositories.book_repository import BookRepository
+
+
+async def test_repository_fails_if_no_async_bind_passed():
+    mocked_sa_manager = MagicMock()
+    mocked_sa_manager.get_bind.return_value = "string_is_not_async_bind"
+    with pytest.raises(UnsupportedBind):
+        repo = BookRepository(sa_manager=mocked_sa_manager)
 
 
 async def test_create_book():
