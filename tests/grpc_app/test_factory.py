2518070b1312519bcfc72cecc22475c2cc6715d9
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-01-28 11:28:50 +0000

    black format

--- tests/grpc_app/test_factory.py
+++ tests/grpc_app/test_factory.py
@@ -7,9 +7,7 @@ from grpc_app import create_server
 
 
 async def test_factory_returns_server():
-    test_config = AppConfig(
-        SQLALCHEMY_CONFIG={}
-    )
+    test_config = AppConfig(SQLALCHEMY_CONFIG={})
 
     with patch("grpc_app.init_storage", return_value=None):
         server = create_server(test_config=test_config)
