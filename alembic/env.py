93c29a1c13205b37a0b580dffbeb8b2b5a037d21
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-03-26 23:32:42 +0100

    Restore wrongly commented lines

--- alembic/env.py
+++ alembic/env.py
@@ -12,7 +12,7 @@ USE_TWOPHASE = False
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-# config = context.config
+config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -33,7 +33,7 @@ init_tables()
 
 target_metadata = sa_manager.get_bind_mappers_metadata()
 db_names = target_metadata.keys()
-# config.set_main_option("databases", ",".join(db_names))
+config.set_main_option("databases", ",".join(db_names))
 
 # add your model's MetaData objects here
 # for 'autogenerate' support.  These must be set
