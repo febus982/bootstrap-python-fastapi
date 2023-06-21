922059477a9041d219a32d8917078f85599e961c
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-06-22 00:19:12 +0100

    Use async logger

--- grpc_app/__init__.py
+++ grpc_app/__init__.py
@@ -1,9 +1,9 @@
-import logging
 import os
 from concurrent import futures
 from typing import Optional
 
 from grpc.aio import server
+from structlog import get_logger
 
 from config import AppConfig, init_logger
 from domains import init_domains
@@ -13,7 +13,7 @@ from storage import init_storage
 
 
 # TODO: Add asyncio support: https://stackoverflow.com/questions/38387443/how-to-implement-a-async-grpc-python-server
-def create_server(test_config: Optional[AppConfig] = None):
+async def create_server(test_config: Optional[AppConfig] = None):
     app_config = test_config or AppConfig()
     init_logger(app_config)
     init_domains(app_config)
@@ -22,13 +22,15 @@ def create_server(test_config: Optional[AppConfig] = None):
     # Register service
     add_BooksServicer_to_server(BooksServicer(), s)
     address = "0.0.0.0:9999"
-    logging.info(f"[{os.getpid()}] Listening on {address}")
+    logger = get_logger()
+    logger.ainfo(f"[{os.getpid()}] Listening on {address}")
     s.add_insecure_port(address)
     return s
 
 
 async def main():  # pragma: no cover
-    s = create_server()
+    s = await create_server()
     await s.start()
-    logging.info("GRPC started")
+    logger = get_logger()
+    await logger.ainfo("GRPC started")
     await s.wait_for_termination()
