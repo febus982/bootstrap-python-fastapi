e8f1403413e51d4f6f849528a3c829d794d199b6
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-06-24 20:33:52 +0100

    Add a test to make sure the model-table binding works correctly

---
+++ tests/storage/tables/test_book_table.py
@@ -0,0 +1,24 @@
+from sqlalchemy_bind_manager._repository import SQLAlchemyAsyncRepository
+
+from domains.books._models import BookModel
+
+
+# This test is to ensure the book table is initialised correctly
+async def test_book_table_works(test_sa_manager):
+    repo = SQLAlchemyAsyncRepository(bind=test_sa_manager.get_bind(), model_class=BookModel)
+    async with repo._get_session() as session:
+        book = await session.get(BookModel, 1)
+        assert book is None
+
+    await repo.save(
+        BookModel(
+            title="pippo",
+            author_name="pluto",
+            book_id=1,
+        )
+    )
+
+    async with repo._get_session() as session:
+        book = await session.get(BookModel, 1)
+        assert book is not None
+        assert book.book_id == 1
