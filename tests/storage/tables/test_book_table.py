179176046f3fcfabc75b5e2d0adc3fabf7edc595
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-06-24 20:35:44 +0100

    Code style

--- tests/storage/tables/test_book_table.py
+++ tests/storage/tables/test_book_table.py
@@ -5,7 +5,9 @@ from domains.books._models import BookModel
 
 # This test is to ensure the book table is initialised correctly
 async def test_book_table_works(test_sa_manager):
-    repo = SQLAlchemyAsyncRepository(bind=test_sa_manager.get_bind(), model_class=BookModel)
+    repo = SQLAlchemyAsyncRepository(
+        bind=test_sa_manager.get_bind(), model_class=BookModel
+    )
     async with repo._get_session() as session:
         book = await session.get(BookModel, 1)
         assert book is None
