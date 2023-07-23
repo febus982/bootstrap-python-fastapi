cd0376787536d15151248a53bd0196d5c6d0eb05
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-07-23 10:42:53 +0100

    Create EventGateway interface and example implementation

---
+++ gateways/event.py
@@ -0,0 +1,15 @@
+from typing import Protocol
+
+from cloudevents.pydantic import CloudEvent
+from structlog import get_logger
+
+
+class EventGatewayInterface(Protocol):
+    async def emit(self, event: CloudEvent) -> None:
+        pass
+
+
+class NullEventGateway:
+    async def emit(self, event: CloudEvent) -> None:
+        logger = get_logger()
+        await logger.ainfo("Event emitted", cloudevent=event)
