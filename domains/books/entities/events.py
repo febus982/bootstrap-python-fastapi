dec1c3e8c5c3bd0259536973f863dd71f65b3a6a
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-08-20 16:19:59 +0100

    Implement Cloudevent HTTP entrypoint

--- domains/books/entities/events.py
+++ domains/books/entities/events.py
@@ -1,6 +1,7 @@
 import typing
 
 import pydantic
+from pydantic import ConfigDict
 
 from domains.books.entities.cloudevent_base import (
     BaseEvent,
@@ -20,3 +21,23 @@ class BookCreatedV1(BaseEvent):
     dataschema: str = dataschema_field("book.created.v1")
 
     data: BookCreatedV1Data
+
+    # The first example is used to generate the OpenAPI documentation!
+    # Examples ate good! Add examples!
+    model_config = ConfigDict(
+        json_schema_extra={
+            "examples": [
+                {
+                    "source": "this.service.url.here",
+                    "type": "book.created.v1",
+                    "dataschema": "book.created.v1/some_event",
+                    "datacontenttype": "text/xml",
+                    "subject": "123",
+                    "data": {"book_id": 0, "title": "string", "author_name": "string"},
+                    "id": "A234-1234-1234",
+                    "specversion": "1.0",
+                    "time": "2018-04-05T17:31:00Z",
+                }
+            ]
+        }
+    )
--- http_app/routes/events.py
+++ http_app/routes/events.py
@@ -1,16 +1,43 @@
 import typing
-from typing import Dict, List
+from typing import Any, Dict, List
 
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Body, Header, HTTPException
 from pydantic import BaseModel
 
 from domains.books.entities.events import BookCreatedV1
 
 router = APIRouter(prefix="/events")
 
+_EVENTS_UNION_TYPE = typing.Union[BookCreatedV1]
+
 
 def _event_registry() -> Dict[str, typing.Type[BaseModel]]:
-    return {"book.created.v1": BookCreatedV1}
+    return {
+        "book.created.v1": BookCreatedV1,
+        # If we find a way to read the class type from here
+        # we can reduce duplication in this way:
+        #
+        # event.type: event
+        # for event in typing.get_args(_EVENTS_UNION_TYPE)
+    }
+
+
+def _event_schema_examples() -> dict[str, dict[str, Any]]:
+    missing_example_message = (
+        "No example has been added to this event but you can"
+        " still explore the event schema. (Ask the developer"
+        " to add an example in the event model if you see this"
+        " message!)"
+    )
+
+    return {
+        k: {
+            "value": getattr(v, "model_config", {})
+            .get("json_schema_extra", {})
+            .get("examples", [missing_example_message])[0]
+        }
+        for k, v in _event_registry().items()
+    }
 
 
 @router.get("/dataschemas/{event}")
@@ -25,3 +52,28 @@ async def event_schema(event: str):
 @router.get("/dataschemas")
 async def event_schema_list() -> List[str]:
     return list(_event_registry().keys())
+
+
+@router.post(
+    "/",
+    openapi_extra={
+        "requestBody": {
+            "content": {
+                "application/cloudevents+json; charset=UTF-8": {
+                    "examples": _event_schema_examples(),
+                }
+            },
+        },
+    },
+    status_code=204,
+)
+async def submit_event(
+    event_data: _EVENTS_UNION_TYPE = Body(
+        media_type="application/cloudevents+json; charset=UTF-8",
+        discriminator="type",
+    ),
+    content_type: typing.Literal[
+        "application/cloudevents+json; charset=UTF-8"
+    ] = Header(),
+) -> None:
+    pass
