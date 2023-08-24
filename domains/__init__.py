8e9d7707c97fb12997c704177b8270297db4c283
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-08-24 16:47:13 +0100

    Create Celery application factory

--- domains/__init__.py
+++ domains/__init__.py
@@ -1,3 +1,4 @@
+from celery import Celery
 from dependency_injector.providers import Object
 
 from config import AppConfig
@@ -8,3 +9,11 @@ def init_domains(config: AppConfig):
     Container(
         config=Object(config),
     )
+
+
+def init_celery(config: AppConfig) -> Celery:
+    celery_app = Celery()
+    celery_app.config_from_object(config.CELERY)
+    celery_app.autodiscover_tasks()
+
+    return celery_app
