0f2b2ed346e605d35d1c38b502213cfb2f7cd0a2
Federico Busetti <729029+febus982@users.noreply.github.com>
2023-11-27 18:12:18 +0000

    Replace aiokafka with confluent-kafka
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- stream_consumer/__init__.py
+++ stream_consumer/__init__.py
@@ -1,43 +1,61 @@
-import asyncio
 from logging import getLogger
+import socket
 
-from aiokafka import AIOKafkaConsumer
+from confluent_kafka import Consumer, KafkaError, KafkaException
 
 from common.bootstrap import application_init
 from common.config import AppConfig
 
 
-async def consume_messages(
-    app_config: AppConfig, shutdown_event: asyncio.Event
-) -> None:
+class KafkaConsumer:
+    def __init__(self, app_config: AppConfig):
+        conf = {
+            "bootstrap.servers": app_config.KAFKA_BOOTSTRAP_SERVERS,
+            "group.id": f"{app_config.APP_NAME}-{socket.gethostname()}",
+            "auto.offset.reset": "smallest",
+        }
+
+        self._consumer = Consumer(conf)
+        self._topics = [app_config.KAFKA_CONSUMER_TOPIC]
+        self._running = False
+        self._logger = getLogger()
+
+    def consume_messages(self):
+        self._running = True
+        try:
+            self._consumer.subscribe(self._topics)
+
+            while self._running:
+                msg = self._consumer.poll(timeout=1.0)
+                if msg is None:
+                    continue
+
+                if msg.error():
+                    if msg.error().code() == KafkaError._PARTITION_EOF:
+                        # End of partition event
+                        self._logger.info("Reached end of offset")
+                        # sys.stderr.write('%% %s [%d] reached end at offset %d\n' %
+                        #                  (msg.topic(), msg.partition(), msg.offset()))
+                    elif msg.error():
+                        raise KafkaException(msg.error())
+                else:
+                    self._logger.info(f"Received message: {msg.value}")
+        finally:
+            # Close down consumer to commit final offsets.
+            self._consumer.close()
+
+
+def run_consumer(app_config: AppConfig):
     application_init(app_config)
     logger = getLogger()
-    if not all([
-        app_config.KAFKA_CONSUMER_TOPIC,
-        app_config.KAFKA_BOOTSTRAP_SERVERS,
-    ]):
+    if not all(
+        [
+            app_config.KAFKA_CONSUMER_TOPIC,
+            app_config.KAFKA_BOOTSTRAP_SERVERS,
+        ]
+    ):
         logger.error("Cannot start consumer without configuration")
         exit(1)
 
-    # This context manager takes care of start() and stop() calls
-    async with AIOKafkaConsumer(
-        app_config.KAFKA_CONSUMER.topic,
-        bootstrap_servers=app_config.KAFKA_CONSUMER.bootstrap_servers,
-        group_id=f"{app_config.APP_NAME}-{app_config.KAFKA_CONSUMER.topic}",
-        # auto_offset_reset='earliest',
-        enable_auto_commit=False,
-    ) as consumer:
-        logger.info("Consumer started")
-        # We want to shutdown if event is triggered
-        while not shutdown_event.is_set():
-            # try to fetch a message with a timeout
-            try:
-                message = await asyncio.wait_for(consumer.getone(), timeout=1.0)
-                logger.info(f"Received message: {message.value.decode('utf-8')}")
-                await asyncio.sleep(10)
-                await asyncio.wait_for(consumer.commit(), timeout=1.0)
-                logger.info("Committed message")
-
-            except asyncio.TimeoutError:
-                # no message fetched within 1 second
-                continue
+    consumer = KafkaConsumer(app_config)
+    consumer.consume_messages()
--- stream_consumer/__main__.py
+++ stream_consumer/__main__.py
@@ -1,21 +1,4 @@
-import asyncio
-import signal
-from logging import getLogger
-
 from common.config import AppConfig
-from stream_consumer import consume_messages
+from stream_consumer import run_consumer
 
-# Event will be set if the program needs to stop
-shutdown_event = asyncio.Event()
-
-
-def signal_handler(*_):
-    logger = getLogger()
-    logger.info("Application received shutdown signal. Stopping...")
-    shutdown_event.set()
-
-
-signal.signal(signal.SIGINT, signal_handler)
-signal.signal(signal.SIGTERM, signal_handler)
-
-asyncio.run(consume_messages(AppConfig(), shutdown_event))
+run_consumer(AppConfig())
