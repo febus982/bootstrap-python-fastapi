3f8fb38a5a726bec2470416e304aa91ed052f8d7
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-06-30 13:22:09 +0100

    Remove celery factory testing. (Can we have support for a factory? :( )
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- src/celery_worker/__init__.py
+++ src/celery_worker/__init__.py
@@ -1,8 +1,15 @@
 """
 This is a tiny layer that takes care  of initialising the shared
 application layers (storage, logs) when running standalone workers
-without having to initialise the HTTP framework (or other ones)
+without having to initialise the HTTP framework (or other ones).
+
+The only operation is assigning the already initalised Celery
+to a global variable because  Celery CLI can't run using a factory
+function.
+
+No need for testing.
 """
+# pragma: no cover
 
 from bootstrap import AppConfig, application_init
 
--- tests/celery_worker/test_app_factory.py
+++
@@ -1,9 +0,0 @@
-from celery import Celery
-from celery_worker import app
-
-
-def test_factory():
-    # Hopefully this is enough to be sure we can run
-    # `celery -A celery_worker.app worker`
-    assert isinstance(app, Celery)
-    assert app.configured
