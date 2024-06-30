2d5bd09509759661e708041b5cc0efc5d26e68d9
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-06-30 17:29:14 +0100

    Inject structlog logger in broker
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- src/bootstrap/faststream.py
+++ src/bootstrap/faststream.py
@@ -1,15 +1,22 @@
 from typing import Any, Dict, Union
 
+import structlog
+
 from domains.events import BaseEvent, get_topic_registry
 from faststream.broker.core.usecase import BrokerUsecase
 from faststream.broker.publisher.proto import PublisherProto
 from faststream.redis import RedisBroker
+from opentelemetry.instrumentation.faststream import RedisOtelMiddleware
 
 from .config import AppConfig
 
 
 def init_broker(config: AppConfig) -> BrokerUsecase[Any, Any]:
-    broker = RedisBroker(config.EVENTS.REDIS_BROKER_URL)
+    broker = RedisBroker(
+        config.EVENTS.REDIS_BROKER_URL,
+        middlewares=(RedisOtelMiddleware,),
+        logger=structlog.getLogger("event_broker")
+    )
 
     return broker
 
