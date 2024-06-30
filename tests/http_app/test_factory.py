76c969fd3922de5929ea97d40c1a081531836c4e
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-06-30 13:24:39 +0100

    Fix http factory tests
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- tests/http_app/test_factory.py
+++ tests/http_app/test_factory.py
@@ -1,9 +1,28 @@
+import os
+from unittest import mock
 from unittest.mock import patch
 
-from bootstrap.config import AppConfig, EventConfig, CeleryConfig
+from bootstrap.config import AppConfig, CeleryConfig, EventConfig
 from http_app import create_app
 
+"""
+We want to run tests without setting local environment variables.
+All tests use test_config from the fixture but I want to test
+the debug setting so we mock the env variables for this test only.
+"""
 
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "OTEL_SERVICE_NAME": "bootstrap-fastapi-worker",
+        "OTEL_EXPORTER_OTLP_ENDPOINT": "http://otel-collector:4317",
+        "CELERY__broker_url": "redis://redis:6379/0",
+        "CELERY__result_backend": "redis://redis:6379/1",
+        "EVENTS__REDIS_BROKER_URL": "redis://redis-events:6379",
+    },
+    clear=True,
+)
 def test_with_default_config() -> None:
     """Test create_app without passing test config."""
     with patch("bootstrap.bootstrap.init_storage", return_value=None):
