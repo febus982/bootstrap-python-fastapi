77233b33e03d0fad6d047a7ccd1bf490cf1241ed
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-06-30 13:31:35 +0100

    Make topic registry filterable
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- src/domains/events/__init__.py
+++ src/domains/events/__init__.py
@@ -1,12 +1,18 @@
-from typing import Collection, Dict
+from typing import Collection, Dict, Optional
 
 from .books import BookCreatedV1, BookUpdatedV1
 from .cloudevent_base import BaseEvent
 
 
-def get_topic_registry() -> Dict[str, Collection[type[BaseEvent]]]:
+def get_topic_registry(
+    topic_filter: Optional[Collection[str]] = None,
+) -> Dict[str, Collection[type[BaseEvent]]]:
     # Ideally we can extract this in a reusable package
     # to reuse it in multiple applications
-    return {
+    registry: Dict[str, Collection[type[BaseEvent]]] = {
         "books_topic": (BookCreatedV1, BookUpdatedV1),
     }
+    if topic_filter:
+        return {k: v for k, v in registry.items() if k in topic_filter}
+    else:
+        return registry
