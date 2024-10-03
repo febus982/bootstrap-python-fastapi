e6851f4326ef27a031ed7139d2b58221f234063d
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-10-03 18:58:23 +0100

    Opentelemetry tweaks (#211)
    * Update dependencies
    
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>
    
    * Instrument celery worker
    
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- src/celery_worker/__init__.py
+++ src/celery_worker/__init__.py
@@ -4,6 +4,15 @@ application layers (storage, logs) when running standalone workers
 without having to initialise the HTTP framework (or other ones)
 """
 
+from celery.signals import worker_process_init
+from opentelemetry.instrumentation.celery import CeleryInstrumentor
+
 from bootstrap import AppConfig, application_init
+
+
+@worker_process_init.connect(weak=False)
+def init_celery_tracing(*args, **kwargs):
+    CeleryInstrumentor().instrument()  # pragma: nocover
+
 
 app = application_init(AppConfig()).celery_app
