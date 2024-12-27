cd9afbf0c1e4acd0c0b4ad7e355f5b8a7d4fac0b
Federico Busetti <729029+febus982@users.noreply.github.com>
2024-12-27 15:18:59 +0000

    Use orjson as dramatiq encoder
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- src/common/dramatiq.py
+++ src/common/dramatiq.py
@@ -1,10 +1,25 @@
-from dramatiq import set_broker
+from dramatiq import set_broker, set_encoder
 from dramatiq.brokers.stub import StubBroker
 from dramatiq.brokers.redis import RedisBroker
+from dramatiq.encoder import Encoder, DecodeError, MessageData
 from dramatiq.middleware import AsyncIO
 from opentelemetry_instrumentor_dramatiq import DramatiqInstrumentor
-
+import orjson
 from .config import AppConfig
+
+
+class ORJSONEncoder(Encoder):
+    """Encodes messages as JSON. orjson is much faster than json.
+    """
+
+    def encode(self, data: MessageData) -> bytes:
+        return orjson.dumps(data)
+
+    def decode(self, data: bytes) -> MessageData:
+        try:
+            return orjson.loads(data)
+        except orjson.JSONDecodeError as e:
+            raise DecodeError("failed to decode message %r" % (data,), data, e) from None
 
 
 def init_dramatiq(config: AppConfig):
@@ -18,3 +33,4 @@ def init_dramatiq(config: AppConfig):
         raise RuntimeError("Running a non-test environment without Redis URL set")
     broker.add_middleware(AsyncIO())
     set_broker(broker)
+    set_encoder(ORJSONEncoder())
