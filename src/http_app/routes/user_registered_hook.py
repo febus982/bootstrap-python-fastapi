bd181c97e0db2af06313a963bac855fbaa390fa2
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-01-04 00:03:28 +0000

    Add comment
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- src/http_app/routes/user_registered_hook.py
+++ src/http_app/routes/user_registered_hook.py
@@ -7,5 +7,6 @@ router = APIRouter(prefix="/user_registered")
 
 @router.post("/")
 async def user_registered(request: Request):
+    # Here we could check the email and add staff metadata to the identity
     logging.info("User registered", extra={"body": await request.json()})
     return {"user_registered": "OK"}
