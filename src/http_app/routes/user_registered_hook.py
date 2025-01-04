e1d4919f23478434d33bf3add50a4b112052abd0
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-01-04 00:06:58 +0000

    Disable coverage on test endpoint
    Signed-off-by: Federico Busetti <729029+febus982@users.noreply.github.com>

--- src/http_app/routes/user_registered_hook.py
+++ src/http_app/routes/user_registered_hook.py
@@ -6,7 +6,7 @@ router = APIRouter(prefix="/user_registered")
 
 
 @router.post("/")
-async def user_registered(request: Request):
+async def user_registered(request: Request):  # pragma: no cover
     # Here we could check the email and add staff metadata to the identity
     logging.info("User registered", extra={"body": await request.json()})
     return {"user_registered": "OK"}
