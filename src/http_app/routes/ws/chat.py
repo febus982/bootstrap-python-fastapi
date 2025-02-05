3679c94e79a6dbd2ac944bb39ffee6846d5ca043
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-02-05 16:33:23 +0000

    Code style

--- src/http_app/routes/ws/chat.py
+++ src/http_app/routes/ws/chat.py
@@ -32,6 +32,20 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+
+@router.websocket("/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
+
 """
 In websocket case we create a server per route.
 If we create other routes we can create more servers
@@ -61,16 +75,3 @@ register_channel_operation(
     messages=[BookUpdatedV1],
     operation_name="ReceiveMessage",
 )
-
-
-@router.websocket("/{client_id}")
-async def websocket_endpoint(websocket: WebSocket, client_id: int):
-    await manager.connect(websocket)
-    try:
-        while True:
-            data = await websocket.receive_text()
-            await manager.send_personal_message(f"You wrote: {data}", websocket)
-            await manager.broadcast(f"Client #{client_id} says: {data}")
-    except WebSocketDisconnect:
-        manager.disconnect(websocket)
-        await manager.broadcast(f"Client #{client_id} left the chat")
