5e85f57c277cbab212b4eec173ea02de54b69bac
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-02-07 21:29:02 +0000

    Run socketio on port 8001

--- src/socketio_app/__main__.py
+++ src/socketio_app/__main__.py
@@ -5,4 +5,4 @@ from common.logs import init_logger
 
 if __name__ == "__main__":
     init_logger(AppConfig())
-    uvicorn.run("socketio_app:create_app", factory=True, host="0.0.0.0", port=8000)
+    uvicorn.run("socketio_app:create_app", factory=True, host="0.0.0.0", port=8001)
