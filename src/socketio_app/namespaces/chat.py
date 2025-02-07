448b1e0b621be433b6f53ac0836f6fd29f8aecbb
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-02-07 22:17:30 +0000

    Create separate services for otel testing

--- src/socketio_app/namespaces/chat.py
+++ src/socketio_app/namespaces/chat.py
@@ -1,4 +1,8 @@
+import logging
+
 import socketio
+
+from common.tracing import trace_function
 
 
 class ChatNamespace(socketio.AsyncNamespace):
@@ -8,5 +12,8 @@ class ChatNamespace(socketio.AsyncNamespace):
     def on_disconnect(self, sid, reason):
         pass
 
+    @trace_function()
     async def on_echo_message(self, sid, data):
+        # Note: this log line is only used to verify opentelemetry instrumentation works
+        logging.info("received message")
         await self.emit("echo_response", data)
