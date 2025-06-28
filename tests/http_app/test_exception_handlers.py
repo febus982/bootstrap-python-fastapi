9f47c79add1f0664a0f701dd58f6c191eec641cc
Federico Busetti <729029+febus982@users.noreply.github.com>
2025-06-28 16:02:31 +0100

    Create `ApplicationError` class and update error handler middleware

--- tests/http_app/test_exception_handlers.py
+++ tests/http_app/test_exception_handlers.py
@@ -3,21 +3,51 @@ from unittest.mock import MagicMock, patch
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
+from common.errors import ApplicationError
+
 
 @patch("logging.exception")
-async def test_exception_is_logged_handler_returns_500(
+async def test_unhandled_exception_is_logged_handler_returns_500(
     mocked_logging_exception: MagicMock,
     testapp: FastAPI,
 ):
     my_exc = Exception("Some random exception")
 
-    @testapp.get("/ppp")
+    @testapp.get("/unhandled_exception")
     async def fake_endpoint():
         raise my_exc
 
     ac = TestClient(app=testapp, base_url="http://test")
-    response = ac.get("/ppp")
+    response = ac.get("/unhandled_exception")
 
     assert response.status_code == 500
     assert response.json() == {"error": "Internal server error"}
     mocked_logging_exception.assert_called_once_with(my_exc)
+
+
+@patch("logging.exception")
+async def test_application_error_is_logged_handler_returns_500(
+    mocked_logging_exception: MagicMock,
+    testapp: FastAPI,
+):
+    my_exc = ApplicationError(
+        public_message="Some random exception",
+        internal_message="Some random internal exception",
+        code="ERR_1234567890",
+        metadata={
+            "user_id": "1234567890",
+        },
+    )
+
+    @testapp.get("/application_error")
+    async def fake_endpoint():
+        raise my_exc
+
+    ac = TestClient(app=testapp, base_url="http://test")
+    response = ac.get("/application_error")
+
+    assert response.status_code == 500
+    assert response.json() == {"error": {"code": "ERR_1234567890", "message": "Some random exception"}}
+    mocked_logging_exception.assert_called_once_with(
+        my_exc.internal_message, extra={"code": my_exc.code, "metadata": my_exc.metadata}
+    )
